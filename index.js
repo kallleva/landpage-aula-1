713a7d0185a8ab961e80933f0bb4fa77b839a192
kalleb vieira <kallebv543@gmail.com>
2024-06-13 23:28:06 -0300

    ajustando variaveis de sistema

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+
+// Use a porta fornecida pelo ambiente, se não estiver disponível use a porta 3000
+const PORT = process.env.PORT || 3000;
 
 // Serve arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota padrão que serve o 'index.html'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Inicia o servidor na porta 3000
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
